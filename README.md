4b14d869f2c4bbe7ff18f9ed211e9df9df0975ae
skorani <samira.korani@gmail.com>
2024-08-07 14:53:23 +0100

    Add pre-commit configuration

---
+++ README.md
@@ -0,0 +1,7 @@
+## Project aim , a simple chain of thought
+
+the project aim is recomanding the best meeting to the ACA meemeeting_data.
+
+Find the best meeting which you need the most.
+
+Run Ollam from this address https://ollama.com/ . llama3.1 will be avalable on your local host 11434 port.
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_python_project',
-    version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="ACALLM",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
-        'requests',  # Add your dependencies here
+        "ollama",
     ],
     entry_points={
-        'console_scripts': [
-            'my_python_project=src.main:main',
+        "console_scripts": [
+            "prompt=src.prompt:prompt",
         ],
     },
 )
